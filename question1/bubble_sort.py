33050b2b70097dc5335009619ad8540d7427a4a2
Jesemeil <otahaghogho@gmail.com>
2023-11-05 16:09:22 +0100

    added a task

---
+++ question1/bubble_sort.py
@@ -0,0 +1,11 @@
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
+
+
+sorted_array = bubble_sort([23, 56, 75, 84, 99, 12, 13])
+print(sorted_array)
